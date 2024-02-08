5d1e9041fc823b792670de112500a19aac187f16
Eokji coding <byunguk98@naver.com>
2024-02-08 14:49:37 +0900

    유저 이름 입력받기
    유저 이름을 입력받으면 h1 태그에 유저 이름을 띄우고 form은 안 보이게 하기.

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 const userForm = document.getElementById("user-form");
 const userInput = userForm.querySelector("input");
-const userButton = userForm.querySelector("button");
+const hello = document.getElementById("hello");
 
-function handleUserBtnClick() {
-  console.log(userInput.value);
-  console.log("clicked!");
+const HIDDEN_CLASSNAME = "hidden";
+
+function onUserSubmit(event) {
+  event.preventDefault();
+  userForm.classList.add(HIDDEN_CLASSNAME);
+  const userName = userInput.value;
+  hello.innerText = `안녕하세요 ${userName}님!`
+  hello.classList.remove(HIDDEN_CLASSNAME);
 }
 
-userButton.addEventListener("click", handleUserBtnClick);
+userForm.addEventListener("submit", onUserSubmit);

67f9f1bb1e8700fadf5cb2f4311058aff6c12bf0
Eokji coding <byunguk98@naver.com>
2024-02-15 20:03:11 +0900

    To Do 추가하기
    li를 만들어 놓고, span을 만들어 li 내부에 추가하도록 했다. 추후 ToDo를 삭제하는 기능도 추가하기 위해서다.

--- js/todo.js
+++ js/todo.js
@@ -2,10 +2,19 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+function addToDo(todos) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  li.appendChild(span);
+  span.innerText = todos;
+  toDoList.appendChild(li);
+}
+
 function handleToDoSubmit(event) {
   event.preventDefault();
   const todos = toDoInput.value;
   toDoInput.value = "";
+  addToDo(todos);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
